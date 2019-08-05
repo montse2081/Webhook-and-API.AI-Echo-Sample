8001e256b263127de8b758dbd54cf51941dd7f5e
montse2081 <52506559+montse2081@users.noreply.github.com>
2019-08-05 11:49:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,18 +17,8 @@ restService.use(
 
 restService.post("/audio", function(req, res) {
   var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-   
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
+  //req.body.result
+  
   return res.json({
    "status": "success",  
   "short_id": "asdass",
